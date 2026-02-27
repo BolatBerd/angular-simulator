58e7a774afa31b8875847cf1352ac67f54036965
Болат Берд <rpyz72@mail.ru>
2026-02-27 16:25:21 +0500

    completed task 3
    выполнил 3 задание

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,31 +1,10 @@
 import { Component, inject } from '@angular/core';
-
-// import { Component, inject } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { CommonModule } from '@angular/common';
-// import { Collection } from '../collection';
-// import { IService } from '../interfaces/IService';
-// import { ITourForm } from '../interfaces/ITourForm';
-// import { IDestination } from '../interfaces/IDestination';
-// import { ITravelBlog } from '../interfaces/ITravelBlog';
-// import { IMessage } from '../interfaces/IMessage';
-// import { IPhotoReport } from '../interfaces/IPhotoReport';
-// import { MessageType } from '../enums/MessageType';
-// import { LocalStorageService } from '../classes/local-storage.service';
-// import { MessageService } from '../classes/message.service';
-// import { IOurService } from '../interfaces/IOurService';
-// import { ITravelEssential} from '../interfaces/ITravelEssential';
-// import { IMessenger } from '../interfaces/IMessenger';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Router } from '@angular/router';
-// import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-header',
   imports: [
-    // FormsModule,
-    // CommonModule,
-    // RouterOutlet,
     RouterLink,
     RouterLinkActive,
   ],
