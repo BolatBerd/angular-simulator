9bff53cb308abbb8faf0a0f7a089fba997e62b32
Болат Берд <rpyz72@mail.ru>
2026-03-30 12:54:05 +0500

    fiaed
    Исправления

--- src/user/user.component.ts
+++ src/user/user.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
-import { IUser } from '../interfaces/IUser';
 import { UserService } from '../classes/user.service';
 import { inject } from '@angular/core';
-import { ObjectUnsubscribedErrorCtor } from 'rxjs/internal/util/ObjectUnsubscribedError';
-import { Observable } from 'rxjs';
-
 
 @Component({
   selector: 'app-user',
