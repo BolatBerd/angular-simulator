3da9cd29d97a21bf5dc5fa15348da504feb26f92
Болат Берд <rpyz72@mail.ru>
2026-05-28 21:29:54 +0500

    fixed
    исправил)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-  path: '',
-  loadComponent: () =>
-    import('../home/home.component').then(
-      m => m.HomeComponent
-    ),
+    path: '',
+    loadComponent: () =>
+      import('../home/home.component').then(
+        m => m.HomeComponent
+      ),
   },
   {
     path: 'user',
