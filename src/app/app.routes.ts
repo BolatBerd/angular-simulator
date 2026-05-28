436e6eafd3bedbf3c04bffe56d7463f7c7d7a72c
Болат Берд <rpyz72@mail.ru>
2026-05-28 21:18:41 +0500

    Fixed
    Исправил

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,25 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('../home/home.component').then(m => m.HomeComponent) },
-  { path: 'user', loadComponent: () => import("../users-page/users-page.component").then(m => m.UsersPageComponent) },
-  { path: '**', loadComponent: () => import('../not-found/not-found.component').then(m => m.NotFoundComponent) }
+  {
+  path: '',
+  loadComponent: () =>
+    import('../home/home.component').then(
+      m => m.HomeComponent
+    ),
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('../users-page/users-page.component').then(
+        m => m.UsersPageComponent
+      ),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('../not-found/not-found.component').then(
+        m => m.NotFoundComponent
+      ),
+  },
 ];
