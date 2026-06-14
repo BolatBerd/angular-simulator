4b34ffb1ff42b97a209a53ffa546d7cabfecf96a
Болат Берд <rpyz72@mail.ru>
2026-06-14 14:39:50 +0500

    fixed
    Исправления

--- src/features/auth/auth.interceptor.ts
+++ src/features/auth/auth.interceptor.ts
@@ -1,49 +1,45 @@
-import { HttpErrorResponse, HttpEvent, HttpEventType, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { catchError, Observable, switchMap, tap, throwError } from 'rxjs';
+import { HttpInterceptorFn, HttpErrorResponse, HttpRequest } from '@angular/common/http';
+import { catchError, switchMap, throwError } from 'rxjs';
 import { MessageService } from '../../classes/message.service';
 import { AuthService } from './auth.service';
 import { inject } from '@angular/core';
 
-export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const messageService: MessageService = inject(MessageService);
   const authService: AuthService = inject(AuthService);
-
   const token: string | null = authService.getAccessToken();
 
-  if (!token) {
-    return next(req);
+  let authReq: HttpRequest<unknown> = req;
+  if (token) {
+    authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
   }
 
-  let authReq: HttpRequest<unknown>  = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
   return next(authReq).pipe(
-    tap((event: HttpEvent<unknown>) => {
-      if (event.type === HttpEventType.Response) {
-        if (event.status === 401) {
-          messageService.showError('Ошибка 401, refreshToken');
-          authService.refreshToken()
-            .pipe(
-              switchMap(() => {
-                const newToken: string | null = authService.getAccessToken();
-                const retryReq: HttpRequest<unknown> = req.clone({
-                  setHeaders: {
-                    Authorization: `Bearer ${newToken}`
-                  }
-                });
-                return next(retryReq);
-              }),
-              catchError((error: HttpErrorResponse) => {
-                authService.logout();
-                return throwError(() => error);
-              })
-            )
-        }
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        messageService.showError('Ошибка 401, refreshToken');
+        return authService.refreshToken().pipe(
+          switchMap(() => {
+            const newToken: string | null = authService.getAccessToken();
+            const retryReq: HttpRequest<unknown> = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${newToken}`
+              }
+            });
+            return next(retryReq);
+          }),
+          catchError((error: HttpErrorResponse) => {
+            authService.logout();
+            messageService.showError('Ошибка');
+            return throwError(() => error);
+          })
+        );
       }
+      return throwError(() => error);
     })
   );
-
 };
